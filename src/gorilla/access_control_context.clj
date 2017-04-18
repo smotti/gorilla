ac7d0896ba4c79a1ad29e6f5465de4cb6d706386
wott <william.ott@medicustek.com>
2017-04-18 16:43:32 +0800

    Add fns to add and remove roles

---
+++ src/gorilla/access_control_context.clj
@@ -0,0 +1,93 @@
+(ns gorilla.access-control-context
+  (:import java.lang.IllegalArgumentException
+           (com.acciente.oacc AccessControlContext AuthorizationException
+                              PasswordCredentials Resource
+                              ResourceCreatePermissions ResourcePermissions
+                              Resources)
+           (com.acciente.oacc.sql SQLProfile SQLAccessControlContextFactory)))
+
+(declare has-create-permissions?)
+
+(def ^:private DEFAULT_DOMAIN "APP_DOMAIN")
+
+(defn authenticate
+  [^AccessControlContext acc id pwd]
+  (.authenticate acc
+                 (Resources/getInstance id)
+                 (PasswordCredentials/newInstance (.toCharArray pwd))))
+
+(defn add-role
+  "
+  Adds a new role with name to the system.
+  Returns the newly created resources on success else nil.
+  "
+  [acc name]
+  (let [accessor (.getSessionResource acc)]
+    (when (has-create-permission? acc accessor "ROLE")
+      (.createResource acc "ROLE" DEFAULT_DOMAIN name))))
+
+(defn has-permission?
+  "
+  Check if accessor has permission on accessed resource.
+
+  Note that we use a set for ResourcePermission, because selecting another
+  method implementation via type hints doesn't work :/.
+  "
+  [acc accessor accessed permission]
+  (try
+    (.hasResourcePermissions acc
+                             accessor
+                             accessed
+                             #{(ResourcePermissions/getInstance permission)})
+    (catch IllegalArgumentException e false)
+    (catch AuthorizationException e false)))
+
+(defn has-create-permission?
+  "
+  Check if accessor has permission to create a resource of resource-class.
+
+  Note that we use a set for ResourceCreatePermission, because selecting another
+  method implementation via type hints doesn't work :/.
+  "
+  [^AccessControlContext acc accessor resource-class]
+  (try
+    (.hasResourceCreatePermissions acc
+                                   accessor
+                                   resource-class
+                                   DEFAULT_DOMAIN
+                                   #{(ResourceCreatePermissions/getInstance "*CREATE")})
+    (catch IllegalArgumentException e false)
+    (catch AuthorizationException e false)))
+
+(defn make-access-control-context
+  ([db-conn sql-profile]
+   (let [schema (condp = sql-profile
+                  :sqlite nil
+                  :postgresql "oacc")
+         _sql-profile (condp = sql-profile
+                        :sqlite (SQLProfile/SQLite_3_8_RECURSIVE)
+                        :postgresql (SQLProfile/PostgreSQL_9_3_RECURSIVE))]
+     (SQLAccessControlContextFactory/getAccessControlContext db-conn schema _sql-profile)))
+  ([db-conn sql-profile auth-provider]
+   (let [schema (condp = sql-profile
+                  :sqlite nil
+                  :postgresql "oacc")
+         _sql-profile (condp = sql-profile
+                        :sqlite "SQLite_3_8_RECURSIVE"
+                        :postgresql "PostgreSQL_9_3_RECURSIVE")]
+     (SQLAccessControlContextFactory/getAccessControlContext db-conn
+                                                             schema
+                                                             _sql-profile
+                                                             auth-provider))))
+
+(defn remove-role
+  "Remove a the role with name. Return true on success and false on failure."
+  [^AccessControlContext acc name]
+  (let [accessor (.getSessionResource acc)
+        role (Resources/getInstance name)]
+    (when (has-permission? acc accessor role "*DELETE")
+      (.deleteResource acc (Resources/getInstance name)))))
+
+(defn unauthenticate
+  [^AccessControlContext acc]
+  (.unauthenticate acc))
