c65f139a456fda0f02e53b93f968ceab8e2279fd
wott <william.ott@medicustek.com>
2017-04-24 09:54:25 +0800

    Add unit test for authenticate

--- test/gorilla/test_access_control_context.clj
+++ test/gorilla/test_access_control_context.clj
@@ -6,14 +6,23 @@
                                            with-sqlite-db]]
             [clojure.test :as t]))
 
-(t/use-fixtures :once with-sqlite-db)
+(t/use-fixtures :each with-sqlite-db)
 
 (t/deftest test-authenticate
   (t/testing "authenticatable admin"
     (let [admin (make-admin "auth-admin")
           acc (make-access-control-ctx)]
-      (println admin)
       (t/is (nil? (sut/authenticate acc (:name admin) (:password admin))))))
-  (t/testing "authenticatable service")
-  (t/testing "unauthenticatable role")
-  (t/testing "authenticatable user"))
+  (t/testing "authenticatable service"
+    (let [service (make-service "auth-service")
+          acc (make-access-control-ctx)]
+      (t/is (nil? (sut/authenticate acc (:name service) (:password service))))))
+  (t/testing "unauthenticatable role"
+    (let [role (make-role "unauth-role")
+          acc (make-access-control-ctx)]
+      (t/is (thrown? IllegalArgumentException
+                     (sut/authenticate (:name role))))))
+  (t/testing "authenticatable user"
+    (let [user (make-user "auth-user")
+          acc (make-access-control-ctx)]
+      (t/is (nil? (sut/authenticate acc (:name user) (:password user)))))))
